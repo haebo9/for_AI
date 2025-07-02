e288e033deff4177de0d20633650496bb3a89998
haebo9 <sjs68as@ajou.ac.kr>
2025-07-02 09:52:32 +0900

    add : V2.1.0 모델을 위한 평가 모델용 코드 저장

--- my_project/Finetuning/model_eval/functions/_type_eval.py
+++ my_project/Finetuning/model_eval/functions/_type_eval.py
@@ -4,7 +4,7 @@ import json
 class TypeEvaluator:
     def __init__(self):
         self.cat_endings = ['냥', '냐옹', '이냥', '이다냥', '다먀', '댜옹']
-        self.dog_endings = ['멍', '~다멍', '~냐왈', '~냐멍', '~다왈', '~다개', '~요멍']
+        self.dog_endings = ['왈', '멍', '~다멍', '~냐왈', '~냐멍', '~다왈', '~다개', '~요멍']
         self.cat_pattern = re.compile("|".join([re.escape(e) for e in self.cat_endings]))
         self.dog_pattern = re.compile("|".join([re.escape(e) for e in self.dog_endings]))
 
