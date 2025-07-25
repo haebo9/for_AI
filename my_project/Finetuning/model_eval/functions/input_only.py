61afcc1d779bd58fba32bf4cb2a098911fc26c01
haebo9 <sjs68as@ajou.ac.kr>
2025-07-25 17:47:32 +0900

    add : 모델 test 데이터완성

--- my_project/Finetuning/model_eval/functions/input_only.py
+++ my_project/Finetuning/model_eval/functions/input_only.py
@@ -1,7 +1,7 @@
 import json
 
-input_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_jsonl/test_general.jsonl"
-output_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_input/input_general.jsonl"
+input_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_input/Lv4.jsonl"
+output_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_input/Lv4.input_only.jsonl"
 
 with open(input_path, "r", encoding="utf-8") as fin, \
      open(output_path, "w", encoding="utf-8") as fout:
@@ -13,3 +13,4 @@ with open(input_path, "r", encoding="utf-8") as fin, \
             "content": data.get("content", "")
         }
         fout.write(json.dumps(out, ensure_ascii=False) + "\n")
+        
--- my_project/Finetuning/model_eval/functions/level_add.py
+++ my_project/Finetuning/model_eval/functions/level_add.py
@@ -20,9 +20,9 @@ def add_level_to_jsonl(input_path: str, output_path: str, level: str):
 
 # ✅ 여기서 직접 설정
 if __name__ == "__main__":
-    input_jsonl_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_input/Lv2.jsonl"          # 원본 JSONL 경로
+    input_jsonl_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_input/Lv4.input_only.jsonl"          # 원본 JSONL 경로
     output_jsonl_path = "/Users/jaeseoksee/Documents/project/for_AI/my_project/Finetuning/model_eval/_input/_input_lv.jsonl"    # 출력 JSONL 경로
-    level = "2"                               # level 값 (문자열 숫자)
+    level = "4"                               # level 값 (문자열 숫자)
 
     add_level_to_jsonl(input_jsonl_path, output_jsonl_path, level)
     print(f"✅ 완료: '{output_jsonl_path}'에 level='{level}'이 추가되었습니다.")
